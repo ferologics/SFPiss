5460b5415165fbd2fd3190804a6a5971b634c91b
ferologics <f.hetes@gmail.com>
2015-09-28 11:50:46 -0700

    Add new methods to the 'Location' protocol as the next goals for the app

--- SFPiss/MapViewController.swift
+++ SFPiss/MapViewController.swift
@@ -14,18 +14,30 @@ protocol Location
 {
     var regionRadius: CLLocationDistance { get }
     func centerMapOnLocation(location: CLLocation)
+    func showCurrentLocation()
+    func reportPiss()
+    func addLocationMarker()
+    func updateLocation()
 }
 
 class MapViewController: UIViewController, MKMapViewDelegate
 {
     @IBOutlet weak var mapView: MKMapView!
-    
+    @IBAction func locate(sender: AnyObject) { showCurrentLocation() }
+    @IBAction func iSmellPiss(sender: AnyObject) {}
     var regionRadius: CLLocationDistance { return 1000 }
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+        self.showCurrentLocation()
+    }
+}
+    
+extension MapViewController:Location
+{
+    func showCurrentLocation()
+    {
         PFGeoPoint.geoPointForCurrentLocationInBackground { (point, error) -> Void in
             if let geoPoint = point
             {
@@ -35,14 +47,27 @@ class MapViewController: UIViewController, MKMapViewDelegate
             } else { print(error) }
         }
     }
-}
     
-extension MapViewController:Location
-{
-    func centerMapOnLocation(location: CLLocation)
+    func updateLocation() //TODO: update the users current location periodically based on movement
     {
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-            regionRadius * 2.0, regionRadius * 2.0)
+        
+    }
+    
+    func reportPiss() //TODO: add piss image at the current location
+    {
+        
+    }
+    
+    func addLocationMarker() //TODO: add a visual representation of the current location of the user
+    {
+        
+    }
+    
+    func centerMapOnLocation(location: CLLocation) // go back to the current location of the user
+    {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance( location.coordinate,
+                                                                   regionRadius * 2.0,
+                                                                   regionRadius * 2.0 )
         mapView.setRegion(coordinateRegion, animated: true)
     }
 }
