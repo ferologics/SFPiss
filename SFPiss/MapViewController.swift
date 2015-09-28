efe83a2cc3715a56058a2c0ab235843e339e4215
mbp_13 <mbp_13@Frantiseks-MacBook-Pro.local>
2015-09-27 18:59:30 -0700

    Add mapkit, Parse frameworks and MapViewController

---
+++ SFPiss/MapViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SFPiss
+//
+//  Created by mbp_13 on 27/09/15.
+//  Copyright © 2015 ferologics. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
